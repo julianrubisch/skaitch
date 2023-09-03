1822351b18c9e73e63dd2613ae10e61128902e74
Julian Rubisch <julian@julianrubisch.at>
2023-09-03 15:18:38 +0200

    chore: Pass sgid passthrough parameter to identify prompt

--- app/jobs/generate_image_job.rb
+++ app/jobs/generate_image_job.rb
@@ -6,6 +6,7 @@ class GenerateImageJob < ApplicationJob
   def perform(prompt:)
     model = Replicate.client.retrieve_model("stability-ai/stable-diffusion-img2img")
     version = model.latest_version
-    version.predict({prompt: prompt.title, image: prompt.data_url}, replicate_rails_url(host: Rails.application.config.action_mailer.default_url_options[:host]))
+    version.predict({prompt: prompt.title, image: prompt.data_url}, replicate_rails_url(host: Rails.application.config.action_mailer.default_url_options[:host],
+                                                                                        params: {sgid: prompt.to_sgid.to_s}))
   end
 end

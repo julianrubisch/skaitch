262775312514a10d1c66a26023861a4dae5cb526
Julian Rubisch <julian@julianrubisch.at>
2023-10-11 09:22:26 +0200

    chore: Broadcast prediction partials

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,4 +1,9 @@
 class Prediction < ApplicationRecord
+  include ActionView::RecordIdentifier
+
+  after_create_commit -> { broadcast_append_later_to :predictions, target: dom_id(prompt, :predictions) }
+  after_update_commit -> { broadcast_replace_later_to self }
+
   belongs_to :prompt
 
   def data_url
